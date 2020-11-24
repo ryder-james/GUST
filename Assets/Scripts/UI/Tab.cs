1410904404ea5a10ba838be0e464c4dfec8e4afc
Ragger15 <carsonsena@gmail.com>
2020-11-23 20:45:43 -0700

    Add outside help

---
+++ Assets/Scripts/UI/Tab.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
+
+public class Tab : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    [SerializeField] public TabControler tabControler;
+    //[SerializeField] public Image image;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabControler.Clicked(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabControler.Enter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabControler.Exit(this);
+    }
+
+    void Start()
+    {
+        //image = GetComponent<Image>();
+        tabControler.Subscribe(this);
+    }
+}
---
+++ Assets/Scripts/UI/TabControler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabControler : MonoBehaviour
+{
+    [SerializeField] public List<Tab> tabs;
+    [SerializeField] public List<GameObject> menus;
+    [SerializeField] public Tab selectedTab;
+    //[SerializeField] public Color idle;
+    //[SerializeField] public Color hover;
+    //[SerializeField] public Color clicked;
+
+    public void Subscribe(Tab tab)
+    {
+        if (tabs == null)
+        {
+            tabs = new List<Tab>();
+        }
+
+        tabs.Add(tab);
+
+    }
+
+    public void Enter(Tab tab)
+    {
+        Reset();
+        //tab.image.tintColor = hover;
+        //Debug.Log(tab.image.tintColor);
+    }
+
+    public void Exit(Tab tab)
+    {
+        Reset();
+    }
+
+    public void Clicked(Tab tab)
+    {
+        selectedTab = tab;
+        Reset();
+        //tab.image.tintColor = clicked;
+        int index = tab.transform.GetSiblingIndex();
+        for (int i = 0; i < menus.Count; i++)
+        {
+            if (i == index)
+            {
+                menus[i].SetActive(true);
+            }
+            else
+            {
+                menus[i].SetActive(false);
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (Tab tab in tabs)
+        {
+            //tab.image.tintColor = idle;
+        }
+    }
+
+}
